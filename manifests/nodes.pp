999f40840c30c628bc3f786920cc82b26f84da5c
Jeff Wong <awole20@gmail.com>
2012-07-27 23:27:06 -0700

    vagrant will be the user for which things are run for

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -9,7 +9,7 @@ node lucid32 {
 	include python
 	include ant
 	class {'ghar':
-		user => 'jwong',
+		user => 'vagrant',
 		repositories => ["https://github.com/awole20/zshrc.git", "https://github.com/awole20/vimrc.git", "https://github.com/awole20/gitconfig.git"],
 	}
 	include erlang
