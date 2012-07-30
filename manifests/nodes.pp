744293df06c37ce68767fd554e07019a8d49385b
Jeff Wong <awole20@gmail.com>
2012-07-30 15:19:10 -0700

    adding node for precise

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -14,3 +14,20 @@ node lucid32 {
 	}
 	include erlang
 }
+
+node precise32 {
+	include apt
+    include baseclass
+	include user
+	include git
+	include vim
+	# include mysql
+	# include apache
+	include python
+	include ant
+	class {'ghar':
+		user => 'vagrant',
+		repositories => ["https://github.com/awole20/zshrc.git", "https://github.com/awole20/vimrc.git", "https://github.com/awole20/gitconfig.git"],
+	}
+	include erlang
+}
