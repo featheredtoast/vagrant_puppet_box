28a2791edb75a45a5b7f3cfeeefaed640dd3e991
Jeff Wong <awole20@gmail.com>
2013-01-29 10:51:16 -0800

    target, chmod

--- modules/logstash/files/logstash.sh
+++ modules/logstash/files/logstash.sh
@@ -2,6 +2,7 @@ sudo wget -O /home/vagrant/logstash.jar https://logstash.objects.dreamhost.com/r
 sudo wget -O /home/vagrant/rabbitmq.tar.gz http://www.rabbitmq.com/releases/rabbitmq-server/v3.0.1/rabbitmq-server-generic-unix-3.0.1.tar.gz
 sudo wget -O /home/vagrant/elasticsearch.tar.gz http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.4.tar.gz
 sudo wget -O /home/vagrant/kibana.tar.gz https://github.com/rashidkpc/Kibana/archive/v0.2.0.tar.gz
-sudo tar -zxvf /home/vagrant/rabbitmq.tar.gz /home/vagrant/
-sudo tar -zxvf /home/vagrant/elasticsearch.tar.gz /home/vagrant/
-sudo tar -zxvf /home/vagrant/kibana.tar.gz /home/vagrant/
+sudo tar -zxvf /home/vagrant/rabbitmq.tar.gz -C /home/vagrant/
+sudo tar -zxvf /home/vagrant/elasticsearch.tar.gz -C /home/vagrant/
+sudo tar -zxvf /home/vagrant/kibana.tar.gz -C /home/vagrant/
+sudo chown -R vagrant:vagrant /home/vagrant
